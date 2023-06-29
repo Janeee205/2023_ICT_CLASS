703db22db62213207bf1c27b656d86024e6c7f10
Janeee205 <ga473625@gmail.com>
2023-06-30 03:38:14 +0900

    🚧 수정중 🚧

--- 230609_LEGO(반응형)실습/js/sidebar-drawer.js
+++
@@ -1,1 +0,0 @@
-document.write('연결?')
--- 230609_LEGO(반응형)실습/js/sidebar.js
+++ 230609_LEGO(반응형)실습/js/sidebar.js
@@ -1,1 +1,19 @@
-document.write('연결!');
+const sidebar = document.querySelector('.sidebar');
+const sidebarMenuButton = document.querySelector('.gnb-icon-button');
+const sidebarOverlay = document.querySelector('.overlay');
+
+// 오버레이
+
+const openSideBar = () => {
+    sidebar.classList.add('is-active');
+    sidebarOverlay.classList.add('is-active');
+}
+
+sidebarMenuButton.addEventListener('click', openSideBar);
+
+const closeSideBar = () => {
+    sidebar.classList.remove('is-active');
+    sidebarOverlay.classList.remove('is-active');
+}
+
+sidebarOverlay.addEventListener('click', closeSideBar);
