b7ae1365a64d08512d81d3bf206bc3a9afbde884
Janeee205 <ga473625@gmail.com>
2023-07-06 00:02:57 +0900

    📝 수업

---
+++ Javascript/230705_alertbox(매개변수)/script.js
@@ -0,0 +1,39 @@
+const alert = document.getElementById('alert');
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('click-btn');
+
+/*
+function openAlert(a) {
+  alert.style.display = a;
+}
+
+function closeAlert(a) {
+  alert.style.display = a;
+}
+*/
+
+
+function openNclose(parameter){
+  alert.style.display = parameter;
+}
+
+
+/**
+document.getElementById('open-btn2').addEventListener('click', function () {
+  document.getElementById('alert2').style.display = 'flex';
+
+})
+
+// document.getElementById('click-btn2').addEventListener('click', function(){
+//   document.getElementById('alert2').style.display = 'none';
+// })
+
+function closeAlert2(){
+  document.getElementById('alert2').style.display = 'none';
+}
+
+document.getElementById('click-btn2').addEventListener('click', closeAlert2);
+ */
+
+// 연습
+
