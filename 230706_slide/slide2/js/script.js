e38a9a635f582202e0ca544c4478a7469f1c40be
Janeee205 <ga473625@gmail.com>
2023-07-07 16:41:11 +0900

    📝 버튼 클릭시 색깔 입히기

--- 230706_slide/slide2/js/script.js
+++ 230706_slide/slide2/js/script.js
@@ -1,45 +1,17 @@
-// transform: translateX(-100vw);
+const buttons = document.querySelectorAll('.btn-group button');
+const slideList = document.querySelector('.slide-list');
 
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
-let btn3 = document.querySelector('.btn3');
+buttons.forEach((btn, index) => {
 
-let btnGroup = document.querySelector('.btn-group');
+  btn.addEventListener('click', () => {
 
-let slideList = document.querySelector('.slide-list');
+    buttons.forEach(btn => {
+      btn.classList.remove('active')
+    })
 
-console.log(slideList)
+    let xValue = -index * 100 + 'vw';
+    slideList.style.transform = `translateX(${xValue})`;
+    btn.classList.toggle('active');
 
-botton1 = () => {
-  slideList.style.transform = 'translateX(0)';
-}
-
-
-// click이벤트 추가하는 방법
-// 1. html onclick ="함수()";
-// 2. 함수 생성 후, 이벤트대상.addEventListener('click', 함수)
-// 3. 이벤트대상.addEventListener('click', function(){}) 콜백;
-
-
-
-btn1.addEventListener('click', botton1);
-
-btn2.addEventListener('click', function(){
-  slideList.style.transform = 'translateX(-100vw)';
+  })
 })
-
-btn3.addEventListener('click', function(){
-  slideList.style.transform = 'translateX(-200vw)';
-})
-
-
-
-// foreach 매개변수
-
-// var transform1 = document.getElementsByTagName('span')[3].style.transform;
-
-/*
-btnGroup.foreach(btn){
-  btn.style.transform = 'translateX(-'+ '00vw)'
-}
-*/
