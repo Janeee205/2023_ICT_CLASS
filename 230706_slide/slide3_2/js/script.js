a5e91082cff6e80dba05ca682a85564dc722cd30
Janeee205 <ga473625@gmail.com>
2023-07-10 22:58:16 +0900

    ✨ prevBtn, nextBtn 누를때마다 버튼색깔 바꾸기

--- 230706_slide/slide3_2/js/script.js
+++ 230706_slide/slide3_2/js/script.js
@@ -6,14 +6,25 @@ let nextBtn = document.querySelector('.next')
 let buttons = document.querySelectorAll('.dot-btn-group button')
 console.log(buttons.length);
 
-
 let currentSlide = 0;
+
+
+changeButtonColor = () => {
+  buttons.forEach((btn, index) => {
+    if (index == currentSlide) {
+      btn.classList.add('active')
+    } else{
+      btn.classList.remove('active')
+    }
+  })
+}
 
 nextBtn.addEventListener('click', function () {
   // slideItem 배열 length의 -1보다 값이 작을 때
   if (currentSlide < slideItems.length - 1) {
     currentSlide++;
     slideList.style.transform = `translateX(-${currentSlide * 100}vw)`
+    changeButtonColor();
   }
 })
 
@@ -21,11 +32,6 @@ prevBtn.addEventListener('click', function () {
   if (currentSlide > 0) {
     currentSlide--;
     slideList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
+    changeButtonColor();
   }
-})
-
-buttons.forEach(function(btn, index){
-console.log(btn, index)
-
-
 })
