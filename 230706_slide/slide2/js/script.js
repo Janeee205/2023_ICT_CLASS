58364aa3cf35bcc4acadac246044dc5883a4f2ad
Janeee205 <ga473625@gmail.com>
2023-07-10 13:12:05 +0900

    💄 button 태그에 active 클래스 추가

--- 230706_slide/slide2/js/script.js
+++ 230706_slide/slide2/js/script.js
@@ -5,12 +5,18 @@ buttons.forEach((btn, index) => {
 
   btn.addEventListener('click', () => {
 
-    buttons.forEach(btn => {
-      btn.classList.remove('active')
+    // buttons.forEach(btn => {
+    //   btn.classList.remove('active')
+    // })
+
+// buttons라는 배열을 한 번 더 돌면서 모든 버튼이 가지고 있는 active class remove
+    buttons.forEach(function(otherBtn){
+      otherBtn.classList.remove('active')
     })
 
     let xValue = -index * 100 + 'vw';
     slideList.style.transform = `translateX(${xValue})`;
+    // 클릭된 버튼에만 active class add
     btn.classList.toggle('active');
 
   })
