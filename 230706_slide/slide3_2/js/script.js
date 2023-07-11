ab214fefb4eabf7763494a8f9abed2fa5a714ee1
Janeee205 <ga473625@gmail.com>
2023-07-11 13:52:05 +0900

    ✅ 수정

--- 230706_slide/slide3_2/js/script.js
+++ 230706_slide/slide3_2/js/script.js
@@ -6,26 +6,32 @@ let nextBtn = document.querySelector('.next')
 let buttons = document.querySelectorAll('.dot-btn-group button')
 console.log(buttons.length);
 
-
 let currentSlide = 0;
+
+
+changeButtonColor = () => {
+  buttons.forEach((btn, index) => {
+    if (index == currentSlide) {
+      btn.classList.add('active')
+    } else{
+      btn.classList.remove('active')
+    }
+  })
+}
 
 nextBtn.addEventListener('click', function () {
   // slideItem 배열 length의 -1보다 값이 작을 때
   if (currentSlide < slideItems.length - 1) {
     currentSlide++;
     slideList.style.transform = `translateX(-${currentSlide * 100}vw)`
-  }
+    changeButtonColor();
+  } 
 })
 
 prevBtn.addEventListener('click', function () {
   if (currentSlide > 0) {
     currentSlide--;
     slideList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
+    changeButtonColor();
   }
-})
-
-buttons.forEach(function(btn, index){
-console.log(btn, index)
-
-
 })
