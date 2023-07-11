28ce280340dbb8b7e11d319d24cb88653d09e0e8
Janeee205 <ga473625@gmail.com>
2023-07-12 00:45:33 +0900

    🚧 작업중 🚧

---
+++ 230711_HSFF실습/js/script.js
@@ -0,0 +1,16 @@
+
+const gnbMenu = document.querySelectorAll('.gnb-list > li');
+
+gnbMenu.forEach(function(item){
+    const subMenu = item.querySelector('.sub');
+
+    item.addEventListener('mouseover', function(){
+        subMenu.classList.add('active');
+        this.querySelector('a').classList.add('on')
+    })
+
+    item.addEventListener('mouseleave', function(){
+        subMenu.classList.remove('active');
+        this.querySelector('a').classList.remove('on')
+    })
+})
