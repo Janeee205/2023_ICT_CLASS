73d0bc68ddf39dc1229402f36831541628911e30
Janeee205 <ga473625@gmail.com>
2023-07-12 16:48:01 +0900

    🚧 작업중 🚧

--- 230711_HSFF실습/js/script.js
+++ 230711_HSFF실습/js/script.js
@@ -1,16 +1,54 @@
-
 const gnbMenu = document.querySelectorAll('.gnb-list > li');
 
-gnbMenu.forEach(function(item){
+gnbMenu.forEach(function (item) {
     const subMenu = item.querySelector('.sub');
 
-    item.addEventListener('mouseover', function(){
+    item.addEventListener('mouseover', function () {
         subMenu.classList.add('active');
         this.querySelector('a').classList.add('on')
     })
 
-    item.addEventListener('mouseleave', function(){
+    item.addEventListener('mouseleave', function () {
         subMenu.classList.remove('active');
         this.querySelector('a').classList.remove('on')
     })
+})
+
+
+// 이미지 슬라이드
+
+const slideList = document.querySelector('.slide-list');
+const spanButton = document.querySelectorAll('.span-wrapper span')
+
+spanButton.forEach((item, index) => {
+    item.addEventListener('click', function () {
+
+        // console.log(item, index)
+        let xValue = -index * 100 + 'vw';
+
+        slideList.style.transform = `translateX(${xValue})`;
+        item.classList.add('active');
+
+        spanButton.forEach(function (otherSpan) {
+            if (otherSpan !== item && otherSpan.classList.contains('active')) {
+                otherSpan.classList.remove('active');
+            }
+        })
+    })
+})
+
+
+// sidebar
+const sidebarButton = document.querySelector('.square-btn');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+
+sidebarButton.addEventListener('click', function(){
+    sidebar.classList.add('is-active');
+    overlay.classList.add('is-active');
+})
+
+overlay.addEventListener('click', () =>{
+    sidebar.classList.remove('is-active');
+    overlay.classList.remove('is-active');
 })
