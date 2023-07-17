9d8c9cf5c0215936e815a5ba8da9679f486d979e
Janeee205 <ga473625@gmail.com>
2023-07-17 13:33:54 +0900

    ✨ .submenu-wrapper slideUp, slideDown

---
+++ 230710_menu/menu2_수업/js/script.js
@@ -0,0 +1,16 @@
+// find() : 자식 밑 하위태그 찾을때
+// children() : 자식요소만 찾을때
+
+// ul을 따로 만들었다면
+// index
+// eq()
+
+
+$('.main-menu li, .submenu-wrapper').hover(function(){
+  // hover시 실행할 함수
+  $('.submenu-wrapper').stop().slideDown(500);
+
+}, function(){ //mouseleave 되었을 때 실행할 함수
+  $('.submenu-wrapper').stop().slideUp(500);
+
+})
