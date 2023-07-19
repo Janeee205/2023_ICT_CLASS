90ab0f323d7798451b8e38e3968625937ea40e2a
Janeee205 <ga473625@gmail.com>
2023-07-19 16:48:06 +0900

    🚧 작업중🚧

---
+++ 230719_img_modal/js/script.js
@@ -0,0 +1,27 @@
+
+// fadein
+// fadeout
+// js로 imgsrc속성 가져오기
+
+const imgList = document.querySelector('.img-item img');
+const popup = document.querySelector('.popup');
+const overlay = document.querySelector('.overlay');
+const bgImg = document.querySelector('.bg-img');
+
+// let text = imgList[3].src;
+console.log(imgList);
+
+/*
+imgList.foreach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(imgAdrr);
+    e.preventDefault();
+    let imgAdrr = imgList.src;
+    overlay.style.display = "block";
+    popup.style.display = "block";
+
+
+    bgImg.style.backgroundImage = "url()"
+  })
+})
+*/
