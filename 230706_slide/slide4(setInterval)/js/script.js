da014e10094125f3c222b739433fcc1f1f34715b
Janeee205 <ga473625@gmail.com>
2023-07-21 00:07:44 +0900

    🚧 작업중 🚧

--- 230706_slide/slide4(setInterval)/js/script.js
+++ 230706_slide/slide4(setInterval)/js/script.js
@@ -3,27 +3,38 @@ const prevBtn = document.querySelector('.slide-arrow .prev');
 const nextBtn = document.querySelector('.slide-arrow .next');
 const slideList = document.querySelector('.slide-list');
 const slideItem = document.querySelectorAll('.slide-list li');
-const slideText = document.querySelector('.slide-text')
+const slideText = document.querySelector('.slide-text');
+const buttons = document.querySelectorAll('.slide-dot-btn button');
 
 let currentSlide = 0
 
 setInterval(() => {
-  if (currentSlide > 0) {
-    currentSlide--;
+  if (currentSlide < slideItem.length - 1) {
+    currentSlide++;
   }
+
   slideList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
   slideText.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
+  btnbgcChange();
+}, 5000)
 
-}, 3000)
+
+btnbgcChange = () => buttons.forEach((btn, index) => {
+  if (index == currentSlide) {
+    btn.classList.add('is-active')
+  } else {
+    btn.classList.remove('is-active')
+  }
+})
+
 
 prevBtn.addEventListener('click', () => {
-
   if (currentSlide > 0) {
     currentSlide--;
   }
   slideList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
   slideText.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
-
+  btnbgcChange();
 })
 
 
@@ -34,4 +45,5 @@ nextBtn.addEventListener('click', () => {
   }
   slideList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
   slideText.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
+  btnbgcChange();
 })
