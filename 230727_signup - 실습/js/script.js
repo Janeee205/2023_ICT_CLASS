b5b44f73f080ad6b1ad6501ad878c8a9d8dde3bc
Janeee205 <ga473625@gmail.com>
2023-07-28 14:08:20 +0900

    ✨ agree 모두 체크시 total 체크

--- 230727_signup - 실습/js/script.js
+++ 230727_signup - 실습/js/script.js
@@ -6,7 +6,6 @@ $('label').on('click', function (e) {
   $(this).children('.checkbox-img').toggleClass('checked');
 
 
-
   // 클릭된 label의 자식인 checkbox-img에
   // checked라는 class가 포함되어 있다면
   // input type = "checkbox"의 속성 checked
@@ -17,4 +16,72 @@ $('label').on('click', function (e) {
   } else {
     $(this).children('input[type="checkbox"]').removeAttr('checked')
   }
+})
+
+
+
+// .total을 체크하면 .agree을 all check
+// .total을 언체크하면 .agree을 전부 uncheck
+
+// .total .checkbox-img가 .checked클래스를 가지고 있는지 확인
+// .agree안 .checkbox-img에 checked class add
+// .agree안 input checkbox를 ckecked ture
+
+// .total .checked-img가 .checked class를 가지고 있지 않다면
+// .agree안 .checkbox-img에 checked class remove
+// .agree안 input checkbox를 ckecked false
+
+$('.total label').on('click', function () {
+  if ($(this).children('.checkbox-img').hasClass('checked')) {
+    $('.agree').find('.checkbox-img').addClass('checked');
+    $('.agree').find('input[type="checkbox"]').attr('checked', true);
+  } else {
+    $('.agree').find('.checkbox-img').removeClass('checked');
+    $('.agree').find('input[type="checkbox"]').removeAttr('checked');
+  }
+})
+
+
+// .agree label 안 checkbox가 모두 체크 상태라면 모두 동의 버튼도 체크로 변경
+// 하나라도 체크되지 않은 값이 있다면 모두 동의 버튼 '체크' -> '언체크'
+$('.agree label').on('click', function () {
+
+  // 전체 체크박스 갯수
+  let len = $('.agree .checkbox-img').length;
+  // 체크 된 체크박수 갯수
+  let chklen = $('.agree .checked').length;
+  // 언체크
+  let unchk = len - chklen;
+
+  console.log(len)
+  console.log('체크된 놈' + chklen)
+
+  // 조건이 참이라면
+  // .total label .checkbox-img checked class add
+  // .total label input checkbox checked true
+
+  // 조건이 참이 아니라면
+  // .total label .checkbox-img checked class remove
+  // .total label input checkbox checked removeAttr
+  if (unchk == 0) {
+    $('.total label .checkbox-img').addClass('checked');
+    $('.total label input[type="checkbox"]').attr('checked', true);
+  } else {
+    $('.total label .checkbox-img').removeClass('checked');
+    $('.total label input[type="checkbox"]').removeAttr('checked');
+  }
+
+
+
+  /*
+  if ((len - chklen) > 0) {
+    $('.total label').find('.checkbox-img').removeClass('checked');
+    $('.total label').find('input[type="checkbox"]').removeAttr('checked');
+  } else{
+    $('.total label').find('.checkbox-img').addClass('checked');
+    $('.total label').find('input[type="checkbox"]').attr('checked', true);
+  }
+*/
+
+
 })
