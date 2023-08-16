7a3d5fc67d5f90912a4f7a7a76917837d5758168
Janeee205 <ga473625@gmail.com>
2023-08-16 12:27:17 +0900

    🐛 뭔가 수정

--- 경주문화관광/header_mainmenu/js/script.js
+++ 경주문화관광/header_mainmenu/js/script.js
@@ -1,44 +1,19 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    arrows: false,
+    variableWidth: true,
+});
 
+// slide
 $('.submenu-list').hide();
 $('.mainmenu-list > li').hover(function () {
     $(this).find('.submenu-list').stop().slideDown(400);
-    // $('.top-menu, .mainmenu').css('backgroundColor', '#FFFFFF');
+    $('.top-menu, .mainmenu, .submenu-list').css('backgroundColor', '#FFFFFF');
 }, function () {
     $(this).find('.submenu-list').stop().slideUp(400);
-    // $('.top-menu, .mainmenu').css('backgroundColor', 'transparent');
+    $('.top-menu, .mainmenu, .submenu-list').css('backgroundColor', 'transparent');
 });
-
-
-
-const backgroundList = document.querySelector('.background-list');
-const slideItem = document.querySelectorAll('.background-list li');
-console.log('개수 ' + slideItem.length);
-let currentSlide = 0
-
-setInterval(() => {
-    if (currentSlide < slideItem.length - 1) {
-        currentSlide++;
-    } else {
-        currentSlide = 0
-    }
-    backgroundList.style.transform = 'translateX(-' + currentSlide * 100 + 'vw)';
-    console.log(currentSlide);
-}, 3000)
-
-
-
-// let cur = 0;
-// let len = $('.slide-item').length;
-
-// function sliding(dir) {
-//     cur = cur + dir
-//     if (cur >= len) {
-//         cur = 0
-//     } else if (cur < 0) {
-//         cur = len - 1;
-//     }
-//     $('.slide-item').eq(cur).siblings('.slide-item').stop().fadeOut();
-// }
-
-// sliding(0);
-// let auto = setInterval(sliding, 5000, 1);
