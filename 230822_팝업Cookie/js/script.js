727dad706b224ed4be4822dfcb09bca620368e14
Janeee205 <ga473625@gmail.com>
2023-08-22 14:36:12 +0900

    💡 Cookie 설명

---
+++ 230822_팝업Cookie/js/script.js
@@ -0,0 +1,17 @@
+// Cookie
+// 1. 클라이언트
+// 2. 서버 - Cookie, section
+// 둘 다 텍스트 형태의 문자로 저장
+
+// 쿠키를 저장하도록 할지 / 말지 => 정보가 서버에 저장됨
+// 사용자가 해당 사이트에서 어떤 행동을 했는지
+// Cookie : 일정기간동안 데이터 값을 브라우저가 내부에 값을 저장한 것
+// 쿠키가 저장되는 방식 name = value 방식
+
+// 개발자 도구 => Application -> Cookies
+// 만료기간을 넣어서 쿠키가 자동 만료 되도록 코드를 작성(UTC)
+// setCookie, getCookie
+// 1. setCookie : 쿠키 값을 설정하는 함수
+// 2. getCookie : 쿠키 값을 가져오는 함수
+
+// local storage를 이용해서 구현 할 수도 있다.
