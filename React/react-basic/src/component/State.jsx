3c499dd578f92f811a0fcf755f350f4eba69eb79
Janeee205 <ga473625@gmail.com>
2023-09-19 16:50:47 +0900

    🤔

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -17,7 +17,18 @@ const State = () => {
   // let num = 0;
 
   let [count, setCount] = useState(0);
-  let [title, setTitle] = useState('리엑트 넘나 어려워용😥');
+  let [title, setTitle] = useState('리액트 넘나 어려워용😥');
+  let [name, setName] = useState(['이름1', '이름2', '이름3']);
+  let [bg, setBg] = useState('white');
+
+  let changeBg = () => {
+    // 삼항연산자로 현재 배경색이 white라면 다른색으로 바꿔주고,
+    // 아니라면 white로 바꾸기
+
+    bg = 'white' ? bg = 'lightblue' : bg = 'white'
+
+  }
+
   // count : 해당 상태값을 의미하는 변수
   // setCount : 해당 상태값을 변경하는 함수
   // 이 함수를 호출할 때 변경된 값을 인수로 전달한다.
@@ -33,10 +44,31 @@ const State = () => {
         // state는 등호로 싱태를 변경할 수 없다.
         // state 변경 함수에는이름 앞에 set을 붙여준다.
         // state 변경 함수로 값을 변경해야 html 재랜더링 한다.
+        // state 변경 함수는 state가 가지고 있는 값을 비교해서 같은 값이면 변경하지 않고, 다른 값이면 state 함수 호출 값으로 변경 시켜준다.
+
         let titleCopy = [...title];
-        titleCopy = '리엑트 넘나 재밌어요!😋'
+        // let titleCopy = title;
+        titleCopy = '리액트 넘나 재밌어요!😋'
+        // console.log(title == titleCopy)
         setTitle(titleCopy);
       }}>제목 변경</button>
+
+      <h3>{name}</h3>
+
+      <button onClick={() => {
+        let nameCopy = [...name];
+        nameCopy[0] = (['김주은'])
+        setName(nameCopy)
+      }}>이름 변경</button>
+
+
+      <button onClick={() => {
+        // setBg('changeBg(bg)')
+        // style={{ backgroundColor: setBg(bg) }}
+      }}
+
+      >배경색 변경</button>
+
 
     </div >
   );
