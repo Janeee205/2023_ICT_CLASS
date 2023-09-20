912e8c7b496cf24920101c73ad3d37fc22dead0a
Janeee205 <ga473625@gmail.com>
2023-09-20 16:45:14 +0900

    📝

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -25,7 +25,8 @@ const State = () => {
     // 삼항연산자로 현재 배경색이 white라면 다른색으로 바꿔주고,
     // 아니라면 white로 바꾸기
 
-    bg = 'white' ? bg = 'lightblue' : bg = 'white'
+    let newBg = bg == 'white' ? 'lightblue' : 'white';
+    setBg(newBg);
 
   }
 
@@ -34,7 +35,11 @@ const State = () => {
   // 이 함수를 호출할 때 변경된 값을 인수로 전달한다.
   // (0) : 초기 상태 값 / 컴포넌트가 처음 렌더링 될 때 사용되는 값
   return (
-    <div>
+    <div style={{ backgroundColor: bg }}>
+
+
+      <button onClick={changeBg}
+      >배경색 변경</button>
 
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Click</button>
@@ -60,15 +65,6 @@ const State = () => {
         nameCopy[0] = (['김주은'])
         setName(nameCopy)
       }}>이름 변경</button>
-
-
-      <button onClick={() => {
-        // setBg('changeBg(bg)')
-        // style={{ backgroundColor: setBg(bg) }}
-      }}
-
-      >배경색 변경</button>
-
 
     </div >
   );
