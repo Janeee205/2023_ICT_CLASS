0b78ce3eb362d3557ee4eaf0cceae260c208f924
Janeee205 <ga473625@gmail.com>
2023-09-25 14:37:05 +0900

    ♻️

---
+++ React/rock/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './App.css';
+import Box from './component/Box';
+
+
+let choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://iheartcraftythings.com/wp-content/uploads/2022/01/6-37.jpg'
+  },
+
+
+  scissor: {
+    name: 'Scissors',
+    img: 'https://images.unsplash.com/photo-1567034866941-5acf937cb310?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1635&q=80'
+  },
+
+  paper: {
+    name: 'Paper',
+    img: 'https://images.unsplash.com/photo-1532153354457-5fbe1a3bb0b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+  }
+
+}
+
+function App() {
+
+
+  let [userSelect, setuserSelect] = useState(null);
+  let [comSelect, setcomSelect] = useState(null);
+  let [result, setResult] = useState('');
+
+  return (
+    <div className="App">
+      <div className="box-list">
+        <Box title="You" />
+        <Box title="Computer" />
+      </div>
+
+      <div className="btn-list">
+        <button type='button'>가위</button>
+        <button type='button'>바위</button>
+        <button type='button'>보</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ React/rock/src/component/Box.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Box.css'
+
+
+const Box = (props) => {
+
+  return (
+    <div className='box-item'>
+      <h1>{props.title}</h1>
+      <h2>Rock</h2>
+      <img src="https://iheartcraftythings.com/wp-content/uploads/2022/01/6-37.jpg" alt="" />
+      <h3>tie</h3>
+    </div>
+
+  )
+};
+
+export default Box;
